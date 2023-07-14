4673d092fbf5e27217e16129b58a4d98a17b8cef
msaylorphila <117216450+msaylorphila@users.noreply.github.com>
2023-07-14 16:59:54 -0400

    validation for email and password hashing added

--- seeds.py
+++ seeds.py
@@ -13,7 +13,7 @@ db.add_all([
   User(username='jwilloughway1', email='rmebes1@sogou.com', password='password123'),
   User(username='iboddam2', email='cstoneman2@last.fm', password='password123'),
   User(username='dstanmer3', email='ihellier3@goo.ne.jp', password='password123'),
-  User(username='djiri4', email='gmidgley4@weather.com', password='password123')
+  User(username='alesmonde0', email='notvalid', password='password123')
 ])
 
 db.commit()
